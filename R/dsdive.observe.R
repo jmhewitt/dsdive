7b04bb36ff5277ad6d792060968a2c48b3bb79fa
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-10-28 22:52:43 -0600

    adding parameter documentation

--- R/dsdive.observe.R
+++ R/dsdive.observe.R
@@ -1,5 +1,9 @@
 #' Simulate observing a fully observed dive trajectory at specified timepoints
 #' 
+#' @param depths Complete record of depth bins visited
+#' @param times Times at which each of \code{depths} was visited
+#' @param t.obs Times at which the trajectory should be observed
+#' 
 #' @example examples/txparams.R
 #' 
 #' @export
