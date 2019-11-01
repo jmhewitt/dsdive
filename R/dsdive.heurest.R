66c771df60bfd9fe269db27d0390660c90833d66
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-11-01 15:32:27 -0600

    heuristic estimation of model parameters, for initializing routines

---
+++ R/dsdive.heurest.R
@@ -0,0 +1,98 @@
+#' Heuristic estimate of model parameters given observations
+#' 
+#' Builds heuristic estimates of dive model parameters, which is useful for 
+#' initializing methods that optimize or explore parameter surfaces and 
+#' posterior distributions.
+#'
+#' @importFrom stringr str_extract_all
+#' 
+#' @param depths Record of observed depth bins
+#' @param times Times at which depth bin observations were made
+#' @param stages.est Vector of guesses for which dive stage the trajectory was 
+#'   in at each observation
+#' @param depth.bins Vector that defines the depth bins (i.e., bin labels)
+#' @param priors Standard deviations for mean-0 normal priors on model 
+#'   parameters, after transformation to real line
+#' @param sub.tx1 Index of minimum depth bin at which transition to intermediate 
+#'   behaviors stage can occur.
+#' 
+#' @example examples/heurest.R
+#' 
+#' @export
+#' 
+#'
+dsdive.heurest = function(depths, times, stages.est, depth.bins, 
+                          priors = rep(5, 12), sub.tx1) {
+  
+  #
+  # linearly impute a complete trajectory
+  #
+  
+  nt = length(times)
+  
+  # impute direct paths, equal time bins, and stages between observations
+  depths.complete = depths[1]
+  durations.complete = c()
+  stages.complete = c()
+  for(i in 2:nt) {
+    # impute direct path between observations
+    path = (depths.complete[length(depths.complete)]:depths[i])[-1]
+    # associate equal time bins for the path
+    np = length(path)
+    path.times = rep(diff(times[(i-1):i]) / np, np)
+    # assign stages to path
+    path.stages = rep(stages.est[i-1], np)
+    # record imputation
+    depths.complete = c(depths.complete, path)
+    durations.complete = c(durations.complete, path.times)
+    stages.complete = c(stages.complete, path.stages)
+  }
+  
+  # finish assembling complete trajectory
+  times.complete = c(0, cumsum(durations.complete))
+  durations.complete = c(durations.complete, NA)
+  stages.complete = c(stages.complete, 3)
+  
+  
+  #
+  # initial parameters for model parameters
+  #
+  
+  # estimate diving rate magnitude between observations (bins/sec)
+  dy = abs(diff(depths) / diff(times))
+  
+  # "moment" estimates for lambda
+  stages.tx = which(diff(stages.est) == 1)
+  lambda.est = c(mean(dy[1:stages.tx[1]]), 
+                 mean(dy[(stages.tx[1]+1):stages.tx[2]]),
+                 mean(dy[-(1:stages.tx[2])]))
+  
+  # "moment" estimate for the probability of transitioning from primary dive
+  sub.tx2 = 1/(
+    min(which(stages.complete == 2)) - min(which(depths.complete >= sub.tx1))
+  )
+  
+  
+  #
+  # optimize initial parameter estimates
+  #
+  
+  o = optim(par = c(rep(0,6), log(lambda.est), qlogis(sub.tx2), rep(0,2)), 
+            fn = function(theta) {
+              dsdive.ld(depths = depths.complete, stages = stages.complete, 
+                        durations = durations.complete, times = times.complete,
+                        beta = matrix(theta[1:6], nrow = 2), 
+                        lambda = exp(theta[7:9]), 
+                        sub.tx = c(sub.tx1, plogis(theta[10])),
+                        surf.tx = theta[11:12], depths.labels = depth.bins) + 
+              sum(dnorm(theta, sd = priors, log = TRUE))
+            }, method = 'BFGS', control = list(fnscale = -1))
+  
+  # package results
+  list(
+    beta = matrix(o$par[1:6], nrow = 2),
+    lambda = exp(o$par[7:9]),
+    sub.tx = c(sub.tx1, plogis(o$par[10])),
+    surf.tx = o$par[11:12]
+  )
+}
