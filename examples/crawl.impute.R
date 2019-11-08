90f1989d1168e64542c2ac95bcc7b3ac467757e6
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-11-08 11:41:04 -0700

    adding crawl-based ctds imputation

---
+++ examples/crawl.impute.R
@@ -0,0 +1,6 @@
+data('dive.sim')
+attach(dive.sim)
+
+imputed = crawl.impute(depth.bins = depth.bins, depths = sim.obs$depths, 
+                       times = sim.obs$times, N = 1, depths.impute = 'midpoint', 
+                       tstep = 5)
