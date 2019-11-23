02449fba540a0b50d9d4508d07f1c16975c65fd0
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-11-23 17:02:55 -0500

    culling old functions

--- examples/fit.fixedimputation.R
+++
@@ -1,7 +0,0 @@
-data('dive.sim')
-attach(dive.sim)
-
-out = fit.fixedimputation(par = params, imputed.list = list(imputed), 
-                          it = 1, burn = 1, verbose = TRUE, t0.dive = 0)
-
-detach(dive.sim)
