a6e07447f60890dfc9cec3a95841ad7259595bf8
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-11-26 09:29:54 -0500

    reparameterizing dive stage transition parameters

--- R/params.convert.R
+++ R/params.convert.R
@@ -19,6 +19,12 @@
 #' 
 params.toList = function(par) {
   
+  sub.tx = exp(par[7:8])
+  sub.tx = c(sub.tx[1] - sub.tx[2], sum(sub.tx))
+  
+  surf.tx = exp(par[9:10])
+  surf.tx = c(surf.tx[1] - surf.tx[2], sum(surf.tx))
+  
   # extract model parameters, and add jacobian for transformations
   res = list(
     # stage 1 downward trend, st. 2 directional persistence, st. 3 upward trend;
@@ -30,11 +36,11 @@ params.toList = function(par) {
     # transition rates, all stages
     lambda = exp(par[4:6]),
     # stage 1->2 transition parameters
-    sub.tx = par[7:8],
+    sub.tx = sub.tx,
     # stage 2->3 transition parameters
-    surf.tx = par[9:10],
+    surf.tx = surf.tx,
     # log-jacobian for transformations
-    logJ = sum(jac.log(x = par[c(1,3,4:6)], log = TRUE))
+    logJ = sum(jac.log(x = par[c(1,3,4:10)], log = TRUE)) + log(2)
   )
   
   res 
@@ -58,5 +64,6 @@ params.toList = function(par) {
 #'
 params.toVec = function(par) {
   c(log(par$beta[1,1]), par$beta[2,2], log(-par$beta[1,3]), log(par$lambda),
-    par$sub.tx, par$surf.tx)
+    log(c(mean(par$sub.tx), diff(par$sub.tx)/2)),
+    log(c(mean(par$surf.tx), diff(par$surf.tx)/2)))
 }
