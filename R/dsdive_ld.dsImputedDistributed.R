c5ef4b51eae954392c487b048336d5f24b1a9eb2
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-12-06 09:17:00 -0500

    (dsdive_cfg) log-density functions

---
+++ R/dsdive_ld.dsImputedDistributed.R
@@ -0,0 +1,25 @@
+#' Evaluate the log-density of a single dive trajectory
+#' 
+#' Evaluates the log-density of a single dive trajectory when the computing 
+#' environment is configured for use on a single core.
+#' 
+#' @param cfg \code{dssingle} object containing dive trajectory data
+#' @param params parameter values at which to evaluate the model's log-density
+#'
+#' @importFrom snow clusterApply
+#' 
+dsdive_ld.dsImputedDistributed = function(cfg, params) {
+  
+  # distribute likelihood computation
+  lds = clusterApply(cl = cfg$cl, x = cfg$ids, fun = function(id) {
+    
+    # extract local config
+    cfg.local = get(x = id, envir = globalenv())
+    
+    # compute and return log-density
+    dsdive_ld(cfg = cfg.local, params = params)
+  })
+  
+  # return total log-density
+  sum(unlist(lds))
+}
