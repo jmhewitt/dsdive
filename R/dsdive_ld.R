704fe7d480d8aa45ebfd1e6ad453dda4e8460840
Josh Hewitt <joshua.hewitt@colostate.edu>
2019-12-06 09:30:29 -0500

    updating log-density documentation; culling old file

--- R/dsdive_ld.R
+++ R/dsdive_ld.R
@@ -1,6 +1,8 @@
 #' Define generic method for evaluating dive model likelihoods under different
 #' computing environments
 #'
+#' All log-densities are evaluted with respect to the parameters on their 
+#' natural scales, vs. log-densities with respect to the transformed scales.
 #' 
 dsdive_ld = function(cfg, params) {
   UseMethod("dsdive_ld")
