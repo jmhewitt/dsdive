53b9bbc2be8b7b6deed112f776cff06401c74090
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-12 15:54:48 -0700

    reinstating the crawl-based imputations

--- examples/crawl.impute.R
+++ examples/crawl.impute.R
@@ -2,7 +2,7 @@ data('dive.sim')
 attach(dive.sim)
 
 imputed = crawl.impute(depth.bins = depth.bins, depths = sim.obs$depths, 
-                       times = sim.obs$times, N = 1, depths.impute = 'midpoint', 
+                       times = sim.obs$times, N = 10, depths.impute = 'midpoint', 
                        tstep = 5)
 
-detach(dive.sim)
+  detach(dive.sim)
