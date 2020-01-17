e0d821bc05e2c62e8b7b342fd9ab4bca3a459b40
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-17 12:41:22 -0500

    adding plotting to example

--- examples/dsdive.fwdsample.fixedstage.R
+++ examples/dsdive.fwdsample.fixedstage.R
@@ -7,5 +7,7 @@ x = dsdive.fwdsample.fixedstage(depth.bins = depth.bins, d0 = 1,
                                 beta = c(.8, .2), lambda = rep(1,3), t0 = 0, 
                                 tf = 100, steps.max = 1e3, s0 = 1)
 
+pl = plot(x = x, depth.bins = depth.bins)
+
 detach(dive.sim$params)
 detach(dive.sim)
