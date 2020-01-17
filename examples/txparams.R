85ff4883971114fcee25d809f318260c8e682739
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-17 11:58:02 -0500

    updating tx.params examples

--- examples/txparams.R
+++ examples/txparams.R
@@ -1,27 +1,7 @@
 data('dive.sim')
 attach(dive.sim)
-attach(dive.sim$params)
 
-dsdive.tx.params(t0 = 0, depth.bins = depth.bins, d0 = 0, 
-                 d0.last = NULL, s0 = 1, beta = beta, lambda = lambda, 
-                 sub.tx = sub.tx, surf.tx = surf.tx, t0.dive = 0, t.stage2 = NA,
-                 model = 'conditional')
+dsdive.tx.params(depth.bins = depth.bins, d0 = 3, s0 = 1, 
+                 beta = c(.8, .2), lambda = rep(1,3))
 
-dsdive.tx.params(t0 = 0, depth.bins = depth.bins, d0 = 60, 
-                 d0.last = 61, s0 = 1, beta = beta, lambda = lambda, 
-                 sub.tx = sub.tx, surf.tx = surf.tx, t0.dive = 0, t.stage2 = NA,
-                 model = 'conditional')
-
-dsdive.tx.params(t0 = 0, depth.bins = depth.bins, d0 = 60, 
-                 d0.last = 61, s0 = 2, beta = beta, lambda = lambda, 
-                 sub.tx = sub.tx, surf.tx = surf.tx, t0.dive = 0, t.stage2 = NA,
-                 model = 'conditional')
-
-dsdive.tx.params(t0 = 0, depth.bins = depth.bins, d0 = 60, 
-                 d0.last = 61, s0 = 2, beta = beta, lambda = lambda, 
-                 sub.tx = sub.tx, surf.tx = surf.tx, t0.dive = 0, 
-                 shift.params = c(50, 1.125, 3, .5), t.stage2 = NA,
-                 model = 'conditional')
-
-detach(dive.sim$params)
 detach(dive.sim)
