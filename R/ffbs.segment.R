a8e75cbd6fa191b7d9b8e034b1b714425cd4d1a9
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-18 17:10:16 -0500

    refactored to handle N=0 and N=1 cases

--- R/ffbs.segment.R
+++ R/ffbs.segment.R
@@ -16,35 +16,54 @@
 #'
 ffbs.segment = function(B, x0, xN, N) {
   
-  # state space size
-  m = nrow(B)
-  
-  # initialize forward-filtering vectors
-  a = vector('list', N+1)
-  
-  # encode initial state distribution
-  a[[1]] = sparseVector(1, x0, m)
-  
-  # forward filter
-  Bt = t(B)
-  for(i in 2:length(a)) {
-    a[[i]] = Bt %*% a[[i-1]]
-    # standardize for numerical stability
-    a[[i]] = a[[i]] / sum(a[[i]])
-  }
-  
-  
-  #
-  # backward sample
-  #
-  
-  s = numeric(N+1)
-  s[1] = x0
-  s[N+1] = xN
-  
-  for(t in N:2) {
-    p = as.numeric(B[,s[t+1]] * a[[t]])
-    s[t] = sample(x = 1:m, size = 1, prob = p)
+  if(N==0) {
+    
+    if(x0==xN) {
+      s = x0
+    } else {
+      stop('Bridging is not possible; N==0, and x0!=xN.')
+    }
+    
+  } else if(N==1) {
+    
+    if(B[x0,xN] > 0) {
+      s = c(x0, xN)
+    } else {
+      stop('Bridging is not possible; N==1, and B indicates P(xN | x0) = 0.')
+    }
+    
+  } else {
+    
+    # state space size
+    m = nrow(B)
+    
+    # initialize forward-filtering vectors
+    a = vector('list', N+1)
+    
+    # encode initial state distribution
+    a[[1]] = sparseVector(1, x0, m)
+    
+    # forward filter
+    Bt = t(B)
+    for(i in 2:length(a)) {
+      a[[i]] = Bt %*% a[[i-1]]
+      # standardize for numerical stability
+      a[[i]] = a[[i]] / sum(a[[i]])
+    }
+    
+    #
+    # backward sample
+    #
+    
+    s = numeric(N+1)
+    s[1] = x0
+    s[N+1] = xN
+    
+    for(t in N:2) {
+      p = as.numeric(B[,s[t+1]] * a[[t]])
+      s[t] = sample(x = 1:m, size = 1, prob = p)
+    }
+    
   }
   
   s
