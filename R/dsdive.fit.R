441c3f659031f73bb8686436585b364773988e8d
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-18 22:32:21 -0500

    gibbs sampler wrapper

---
+++ R/dsdive.fit.R
@@ -0,0 +1,62 @@
+#' Likelihood for completely observed dive trajectories
+#'
+#' @param depths Depth bin indices in which the trajectory was observed
+#' @param durations Amount of time spent in each depth bin
+#' @param times Times at which each depth bin was entered
+#' @param stages Record of dive stages at each depth bin
+#' @param beta \eqn{2 x 3} matrix in which each column contains the diving 
+#'  preference and directional persistence parameters for the DIVING, SUBMERGED, 
+#'  and SURFACING dive stages.
+#' @param lambda length 3 vector that specifies the transition rate, 
+#'   respectively in the DIVING, SUBMERGED, and SURFACING stages.
+#' @param sub.tx length 2 vector that specifies the first depth bin at which 
+#'   transitions to the SUBMERGED stage can occur and the probability that such 
+#'   a transition occurs at the next depth transition
+#' @param surf.tx parameter that specifies the probability the trajectory will 
+#'   transition to the SURFACING stage at the next depth transition
+#' @param depth.bins \eqn{n x 2} Matrix that defines the depth bins.  The first 
+#'   column defines the depth at the center of each depth bin, and the second 
+#'   column defines the half-width of each bin.
+#' @param t0.dive Time at which dive started
+#' @param d0.last If the depth bin that proceeded the first depth bin in 
+#'   \code{depths}.  If the trajectory to be analyzed was started at the 
+#'   surface, then set \code{c0.last=NULL}.
+#' @param model Either \code{"conditional"} or \code{"logit"} depending on the 
+#'   method used to determine stage transition probability curves
+#' 
+#' @example examples/ld.R
+#' 
+#' @importFrom stats dexp dbinom
+#' 
+#' @export
+#' 
+dsdive.fit = function(
+  dives.obs, cl, beta.init, lambda.init, verbose = FALSE, T1.prior, T2.prior, 
+  pi1.prior, pi2.prior, lambda1.prior, lambda2.prior, lambda3.prior, it, 
+  inflation.factor.lambda = 1.1, checkpoint.interval = Inf, 
+  checkpoint.function = function(x, ...) {} ) {
+  
+  impute = function(depth.bins, depths, times, beta, lambda, t.stages) {
+    dsdive.impute(
+      depth.bins = depth.bins, depths = depths, times = times, beta = beta, 
+      lambda = lambda, inflation.factor.lambda = inflation.factor.lambda, 
+      verbose = FALSE, t.stages = t.stages, method.N = 'exact', N.max = 20)
+  }
+  
+  imp.gibbs = function(depth.bins, depths, times, beta, lambda, imputed.cond) {
+    impute(depth.bins, depths, times, beta, lambda, imputed.cond$t.stages)
+  }
+  
+  res = dsdive.gibbs(dives.obs = dives.obs, cl = cl, impute.init = impute, 
+                     impute.gibbs = imp.gibbs,
+                     init = list(beta = beta.init, lambda = lambda.init), 
+                     verbose = verbose, maxit = it, T1.prior = T1.prior, 
+                     T2.prior = T2.prior, pi1.prior = pi1.prior, 
+                     pi2.prior = pi2.prior, lambda1.prior = lambda1.prior, 
+                     lambda2.prior = lambda2.prior, 
+                     lambda3.prior = lambda3.prior, 
+                     checkpoint.fn = checkpoint.fn, 
+                     checkpoint.interval = checkpoint.interval)
+   
+  res 
+}
