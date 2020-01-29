f07aebcff63615f85465f16b14e0e0af8885c674
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-29 13:35:41 -0700

    making function more robust

--- R/refine.partition.R
+++ R/refine.partition.R
@@ -8,19 +8,24 @@
 #' 
 refine.partition = function(breaks, max.width) {
   
-  c(do.call(c, sapply(1:(length(breaks)-1), function(i) {
-    # width of i^th interval
-    w = diff(breaks[i + 0:1])
-    if(w <= max.width) {
-      breaks[i]
-    } else {
-      # ceiling(w/max.width) == number of internal breakpoints, we add 2 to 
-      # n to account for the start and end points
-      n = ceiling(w/max.width) + 2
-      # but we do not return the endpoint because this is included in the next
-      # segment
-      seq(from = breaks[i], to = breaks[i+1], length.out = n)[1:(n-1)]
-    }
-  })), breaks[length(breaks)])
+  n = length(breaks)
   
+  do.call(c, lapply(1:n, function(i) {
+    if(i==n) {
+      breaks[n]
+    } else {
+      # width of i^th interval
+      w = diff(breaks[i + 0:1])
+      if(w <= max.width) {
+        breaks[i]
+      } else {
+        # ceiling(w/max.width) == number of internal breakpoints, we add 2 to 
+        # n to account for the start and end points
+        n = ceiling(w/max.width) + 2
+        # but we do not return the endpoint because this is included in the next
+        # segment
+        seq(from = breaks[i], to = breaks[i+1], length.out = n)[1:(n-1)]
+      }
+    }
+  }))
 }
