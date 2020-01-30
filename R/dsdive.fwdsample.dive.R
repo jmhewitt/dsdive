21baa27d75bdbb229d8e45f7d8c1b37151a4cd7e
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-01-30 14:20:53 -0700

    allow stage transitions within depth bins in forward sampling of model

--- R/dsdive.fwdsample.dive.R
+++ R/dsdive.fwdsample.dive.R
@@ -67,31 +67,36 @@ dsdive.fwdsample.dive = function(depth.bins, beta, lambda, t0, steps.max,
                                    beta = beta, lambda = lambda, t0 = t0, 
                                    tf = tf1, steps.max = steps.max, s0 = 1)
   
+  # adjust dive s.t. we stop stage 1 dynamics when stage 1 ends
+  n1 = length(d1$depths)
+  d1$durations[n1] = tf1 - d1$times[n1]
+  
   #
   # sample stage 2 portion of dive
   #
   
-  n1 = length(d1$depths)
-  
-  tf2 = d1$times[n1] + d1$durations[n1] + T2
+  tf2 = tf1 + T2
   d2 = dsdive.fwdsample.fixedstage(depth.bins = depth.bins, 
                                    beta = beta, lambda = lambda, 
                                    d0 = d1$depths[n1],  
-                                   dur0 = d1$durations[n1],
-                                   t0 = d1$times[n1], 
+                                   dur0 = NULL,
+                                   t0 = tf1, 
                                    tf = tf2, steps.max = steps.max, s0 = 2)
+  
+  
+  # adjust dive s.t. we stop stage 2 dynamics when stage 2 ends
+  n2 = length(d2$depths)
+  d2$durations[n2] = tf2 - d2$times[n2]
   
   #
   # sample stage 3 portion of dive
   #
   
-  n2 = length(d2$depths)
-  
   d3 = dsdive.fwdsample.fixedstage(depth.bins = depth.bins, 
                                    beta = beta, lambda = lambda, 
                                    d0 = d2$depths[n2],  
-                                   dur0 = d2$durations[n2],
-                                   t0 = d2$times[n2], 
+                                   dur0 = NULL,
+                                   t0 = tf2, 
                                    tf = Inf, steps.max = steps.max, s0 = 3)
   
   #
@@ -99,9 +104,9 @@ dsdive.fwdsample.dive = function(depth.bins, beta, lambda, t0, steps.max,
   #
   
   res = list(
-    depths = c(d1$depths, d2$depths[-1], d3$depths[-1]),
-    times = c(d1$times, d2$times[-1], d3$times[-1]),
-    stages = c(d1$stages, d2$stages[-1], d3$stages[-1])
+    depths = c(d1$depths, d2$depths, d3$depths),
+    times = c(d1$times, d2$times, d3$times),
+    stages = c(d1$stages, d2$stages, d3$stages)
   )
   
   res$durations = c(diff(res$times), Inf)
