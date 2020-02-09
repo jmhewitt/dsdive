de5a6bb674d7b0f91e04fe0fbab5165cd958a508
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-02-09 15:29:53 -0700

    updating data and simulation script

--- data_gen/sim_gen.R
+++ data_gen/sim_gen.R
@@ -18,9 +18,23 @@ lambda = c(1, .3, .5)
 # simulate and observe a dive
 #
 
+# mean and sd. for time in stage
+T1.mean = 10
+T1.sd = 2
+T2.mean = 15
+T2.sd = 2
+
+# convert mean and sd. for time in stage to gamma shape/rate parameters
+T1.params = c(T1.mean^2/T1.sd^2, T1.mean/T1.sd^2)
+T2.params = c(T2.mean^2/T2.sd^2, T2.mean/T2.sd^2)
+
+# sample stage durations
+T1 = 60 * rgamma(n = 1, shape = T1.params[1], rate = T1.params[2])
+T2 = 60 * rgamma(n = 1, shape = T2.params[1], rate = T2.params[2])
+
 # simulate dive
 x = dsdive.fwdsample.dive(depth.bins = depth.bins, beta = beta, lambda = lambda, 
-                          t0 = 0, steps.max = 1e3, T1 = 15*60, T2 = 12*60)
+                          t0 = 0, steps.max = 1e3, T1 = T1, T2 = T2)
 
 # observe dive at regular time intervals
 obs = dsdive.observe(depths = x$depths, times = x$times, stages = x$stages, 
