282622e81d54875ece6a307e57bdab9b7895d1e8
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-02-26 17:58:09 -0700

    initial commit of dive offset sampling; revising checkpoint function format

--- R/dsdive.gibbs.obs.R
+++ R/dsdive.gibbs.obs.R
@@ -32,7 +32,8 @@ dsdive.gibbs.obs = function(
   dsobs.list, t.stages.list, beta.init, lambda.init, verbose = FALSE, maxit, 
   checkpoint.fn, checkpoint.interval = 3600, pi1.prior, pi2.prior, 
   lambda1.prior, lambda2.prior, lambda3.prior, tstep, depth.bins, 
-  T1.prior.params, T2.prior.params, max.width) {
+  T1.prior.params, T2.prior.params, max.width, max.width.offset,
+  t0.prior.params) {
 
   
   n = length(dsobs.list)
@@ -40,15 +41,20 @@ dsdive.gibbs.obs = function(
   lambda.priors.list = list(lambda1.prior, lambda2.prior, lambda3.prior)
   beta.priors.list = list(pi1.prior, pi2.prior)
   
+  # copy of dive observations with depths and timepoints aligned with t0=0
+  dsobs.aligned = dsobs.list
+  
   
   #
   # initialize sampler state and output
   #
   
   theta = list(beta = beta.init, lambda = lambda.init)
+  offsets = numeric(n)
   
   trace = matrix(NA, nrow = maxit, ncol = 5)
   trace.t.stages = vector('list', length = maxit)
+  trace.offsets = matrix(0, nrow = maxit, ncol = n)
   colnames(trace) = c('pi1', 'pi2', 'lambda1', 'lambda2', 'lambda3')
   
   tick.checkpoint = proc.time()[3]
@@ -79,7 +85,7 @@ dsdive.gibbs.obs = function(
     
     # update stage 1 parameters
     theta = dsdive.obs.sampleparams(
-      dsobs.list = dsobs.list, t.stages.list = t.stages.list, P.raw = P.raw, 
+      dsobs.list = dsobs.aligned, t.stages.list = t.stages.list, P.raw = P.raw, 
       s0 = 1, depth.bins = depth.bins, beta = theta$beta, lambda = theta$lambda, 
       lambda.priors.list = lambda.priors.list, 
       beta.priors.list = beta.priors.list, tstep = tstep)
@@ -90,7 +96,7 @@ dsdive.gibbs.obs = function(
                                      tstep = tstep, include.raw = TRUE)
     # update stage 2 parameters
     theta = dsdive.obs.sampleparams(
-      dsobs.list = dsobs.list, t.stages.list = t.stages.list, P.raw = P.raw, 
+      dsobs.list = dsobs.aligned, t.stages.list = t.stages.list, P.raw = P.raw, 
       s0 = 2, depth.bins = depth.bins, beta = theta$beta, lambda = theta$lambda, 
       lambda.priors.list = lambda.priors.list, 
       beta.priors.list = beta.priors.list, tstep = tstep)
@@ -102,7 +108,7 @@ dsdive.gibbs.obs = function(
     
     # update stage 3 parameters
     theta = dsdive.obs.sampleparams(
-      dsobs.list = dsobs.list, t.stages.list = t.stages.list, P.raw = P.raw, 
+      dsobs.list = dsobs.aligned, t.stages.list = t.stages.list, P.raw = P.raw, 
       s0 = 3, depth.bins = depth.bins, beta = theta$beta, lambda = theta$lambda, 
       lambda.priors.list = lambda.priors.list, 
       beta.priors.list = beta.priors.list, tstep = tstep)
@@ -116,30 +122,44 @@ dsdive.gibbs.obs = function(
       print(theta)
     }
     
-    #
-    # update stage transition time parameters
-    #
     
+    #
+    # update stage transition time parameters and dive offsets
+    #
     
     for(i in 1:n) {
-      d = dsobs.list[[i]]
+      
+      # sample dive stage transition times
+      d = dsobs.aligned[[i]]
       t.stages.list[[i]] = dsdive.obs.sample.stages(
         depths = d$depths, times = d$times, t.stages = t.stages.list[[i]], 
         P.raw = P.raw, T.range = c(d$times[1], d$times[length(d$times)]), 
         depth.bins = depth.bins, T1.prior.params = T1.prior.params, 
         T2.prior.params = T2.prior.params, max.width = max.width, 
         debug = FALSE)$t.stages
+      
+      # sample new offsets
+      if(!is.null(t0.prior.params)) {
+        d0 = dsdive.obs.sample.offsets(
+          dsobs.aligned = d, dsobs.unaligned = dsobs.list[[i]], 
+          offset = offsets[i], t.stages = t.stages.list[[i]], P.raw = P.raw, 
+          depth.bins = depth.bins, tstep = tstep, max.width = max.width.offset, 
+          t0.prior.params = t0.prior.params)
+        # extract new offsets
+        dsobs.aligned[[i]] = d0$dsobs.aligned
+        offsets[i] = d0$offset
+      }
+      
     }
-    
     
     
     #
     # save trace
     #
     
-    # save trace 
     trace[it,1:5] = unlist(theta[1:2])
     trace.t.stages[[it]] = t.stages.list
+    trace.offsets[it,] = offsets
     
     tock = proc.time()[3]
     
@@ -157,7 +177,9 @@ dsdive.gibbs.obs = function(
       if(verbose) {
         message('--- Checkpoint ---')
       }
-      checkpoint.fn(trace = trace[1:it,], trace.t.stages = trace.t.stages[1:it])
+      checkpoint.fn(list(trace = trace[1:it,], 
+                         trace.t.stages = trace.t.stages[1:it], 
+                         trace.offset = trace.offsets[1:it,]))
       tick.checkpoint = proc.time()[3]
     }
     
@@ -165,6 +187,7 @@ dsdive.gibbs.obs = function(
   
   list(
     theta = trace,
-    trace.t.stages = trace.t.stages
+    trace.t.stages = trace.t.stages,
+    trace.offsets = trace.offsets
   )
 }
