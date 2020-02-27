ebc21ad74e6b5b013f8e50bee465516462b13d58
Josh Hewitt <joshua.hewitt@colostate.edu>
2020-02-26 17:36:38 -0700

    initial commit of dive offset sampler

---
+++ R/dsdive.obs.sample.offsets.R
@@ -0,0 +1,116 @@
+#' Sample stage transition times from full conditional posterior
+#' 
+#' We assume the prior distributions for the stage transition times are Gamma 
+#' distributions with shape/rate parameterization.
+#' 
+#' @example examples/dsdive.obs.sample.offsets.R
+#' 
+#' 
+dsdive.obs.sample.offsets = function(dsobs.aligned, dsobs.unaligned, offset, 
+                                     t.stages, P.raw, depth.bins, tstep,
+                                     max.width, t0.prior.params, 
+                                     debug = FALSE) {
+  
+  tstep2 = 2*tstep
+  
+  #
+  # components for constructing proposal densities for full conditionals
+  # 
+  
+  align.obs = function(eps) {
+    # construct a dsobs object with realigned observation times
+    #
+    # Parameters:
+    #   eps - offset
+    
+    # realign observation times with beginning of dive
+    times = dsobs.unaligned$times - eps
+    
+    # remove observations from before the dive begins
+    nonneg.times = times >= 0
+    times = times[nonneg.times]
+    depths = dsobs.unaligned$depths[nonneg.times]
+    
+    # dives start at the surface; add this if necessary
+    if(times[1] > 0) {
+      times = c(0, times)
+      depths = c(1, depths)
+    }
+    
+    # package dive
+    aligned = list(
+      times = times,
+      depths = depths,
+      stages = findInterval(times, t.stages) + 1
+    )
+    class(aligned) = 'dsobs'
+    
+    aligned
+  }
+  
+  
+  lp = function(eps) { sapply(eps, function(eps) {
+    # log-posterior for data given a dive offset
+    # 
+    # Parameters: 
+    #  eps - offset
+    
+    # construct dsobs object with realigned observation times
+    aligned = align.obs(eps)
+    
+    # log density associated with dive offset
+    ld = dsdive.obsld(dsobs.list = aligned, t.stages.list = t.stages, 
+                      P.raw = P.raw, s0 = 1, sf = 3)
+    
+    # return log-posterior (ignoring jacobian)
+    lp = ld + dbeta(x = (eps + tstep)/tstep2, shape1 = t0.prior.params[1],
+                    shape2 = t0.prior.params[2], log = TRUE)
+  })}
+  
+  
+  #
+  # sample dive offset
+  #
+  
+  # determine intervals and midpoints for log-quadratic sampling envelope
+  breaks = refine.partition(
+    breaks = c(-tstep, tstep),
+    max.width = max.width)
+  anchors = breaks[1:(length(breaks)-1)] + diff(breaks)/2
+  
+  # evaluate log-posterior at anchor points
+  anchors.extra = c(anchors, breaks[length(breaks)])
+  lp.eval = lp(eps = anchors.extra)
+
+  # build envelope
+  q1 = envelope.logquad(breaks = breaks, logf = lp.eval[1:length(anchors)],
+                        d.logf = diff(lp.eval)/diff(anchors.extra),
+                        dd.logf.sup = rep(0, length(anchors)),
+                        anchors = anchors)
+  
+  # draw proposal
+  prop = q1$rquad(n = 1)
+
+  # compute metropolis ratio (as an independence sampler)
+  lR = (lp(eps = prop) - q1$dquad(x = prop, log = TRUE)) -
+       (lp(eps = offset) - q1$dquad(x = offset, log = TRUE))
+
+  # accept/reject
+  if(log(runif(1)) <= lR) {
+    offset = prop
+    dsobs.aligned = align.obs(eps = offset)
+  }
+  
+  
+  #
+  # Package results
+  #
+
+  res = list(offst = offset, dsobs.aligned = dsobs.aligned)
+  
+  if(debug == TRUE) {
+    res$debug = list(lp = lp, q1 = q1)
+  }
+  
+  res
+}
---
+++ examples/dsdive.obs.sample.offsets.R
@@ -0,0 +1,38 @@
+data('dive.sim')
+attach(dive.sim)
+attach(dive.sim$params)
+
+devtools::document()
+
+# extract true stage transition times
+t.stages = sim$times[c(FALSE, diff(sim$stages)==1)]
+
+# build probability matrices for observations
+tstep = diff(sim.obs$times[1:2])
+P.raw = lapply(1:3, function(s) {
+  dsdive.obstx.matrix(depth.bins = depth.bins, beta = params$beta, 
+                      lambda = params$lambda, s0 = s, tstep = tstep, 
+                      include.raw = TRUE)
+})
+
+# sample new offsets
+res = dsdive.obs.sample.offsets(
+  dsobs.aligned = sim.obs, dsobs.unaligned = sim.obs, offset = 0, 
+  t.stages = t.stages, P.raw = P.raw, depth.bins = depth.bins, tstep = tstep,
+  t0.prior.params = c(1,1), max.width = 15, debug = TRUE)
+
+
+par(mar = c(5, 5, 4, 2) + .1)
+xmin = -tstep
+xmax = tstep
+# plot un-normalized full conditional posterior for offset
+curve((res$debug$lp(eps = x)), 
+      from = xmin, to = xmax, n = 1e3, col = 'grey60',
+      xlab = expression(epsilon), ylab = expression(ln~f(epsilon~'|'~...)))
+# overlay sampling envelope
+curve((res$debug$q1$e.log(x = x)), from = xmin, to = xmax, 
+      add = TRUE, col = 2, n = 1e3)
+
+
+detach(dive.sim$params)
+detach(dive.sim)
