4f574e8d7a5a2592f3089a6a5fd43b6113996504
Josh Hewitt <joshua.hewitt@duke.edu>
2020-03-17 15:52:44 -0600

    adding end-of-dive offset to align code

--- R/dsdive.align.obs.R
+++ R/dsdive.align.obs.R
@@ -11,7 +11,7 @@
 #' 
 #' @export
 #' 
-dsdive.align.obs = function(depths, times, t.stages, offset) {
+dsdive.align.obs = function(depths, times, t.stages, offset, offset.tf) {
   
   # realign observation times with beginning of dive
   times.aligned = times - offset
@@ -27,6 +27,14 @@ dsdive.align.obs = function(depths, times, t.stages, offset) {
     depths.aligned = c(1, depths.aligned)
   }
   
+  # identify observations at or after when the dive ends
+  tf = times.aligned[length(times.aligned)] - offset.tf
+  times.within = times.aligned < tf
+  
+  # remove observations after the dive end and add final surface obs
+  times.aligned = c(times.aligned[times.within], tf)
+  depths.aligned = c(depths.aligned[times.within], 1)
+  
   #
   # package dive
   #
@@ -34,7 +42,7 @@ dsdive.align.obs = function(depths, times, t.stages, offset) {
   aligned = list(
     times = times.aligned,
     depths = depths.aligned,
-    stages = findInterval(times.aligned, t.stages) + 1
+    stages = findInterval(times, t.stages) + 1
   )
   
   class(aligned) = 'dsobs'
--- examples/dsdive.align.obs.R
+++ examples/dsdive.align.obs.R
@@ -5,7 +5,8 @@ attach(params)
 t.stages = sim$times[c(FALSE, diff(sim$stages)==1)]
   
 dsdive.align.obs(depths = sim.obs$depths, times = sim.obs$times, 
-                 t.stages = t.stages, offset = 401)
+                 t.stages = t.stages, offset = -300, 
+                 offset.tf = -300)
 
 detach(params)
 detach(dive.sim)
