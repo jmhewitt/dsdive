04b880355ae844226f5ec8f093a8512b7aa56c3a
Josh Hewitt <joshua.hewitt@duke.edu>
2020-05-20 16:14:31 -0600

    update documentation

--- R/dsdive.obstx.matrix_interpolator.R
+++ R/dsdive.obstx.matrix_interpolator.R
@@ -1,25 +1,22 @@
-#' Compute probability transition matrix for partially observed CTMC
+#' Interpolators for probability transition matrices of partially observed CTMC
 #' 
-#' Given model parameters, this function will compute the probability transition 
-#' matrix for a Continuous time Markov chain (CTMC) that is observed once, and 
-#' then again at a time of \code{tstep} units of time later.  If 
-#' \code{include.raw==TRUE}, then raw components of the probability transition 
-#' matrix will be returned so that the transition matrix can be computed for 
-#' arbitrary timesteps.
+#' Given a range of model parameters, this function will compute the 
+#' probability transition matrices for all combinations of a Continuous time 
+#' Markov chain (CTMC) that is observed once, and  then again at a time of 
+#' \code{tstep} units of time later.  Then, thin plate spline interpolating 
+#' functions will be built to allow probability transition matrix entries to 
+#' be approximated at different values of the input parameters.
 #' 
 #' @param depth.bins \eqn{n x 2} Matrix that defines the depth bins.  The first 
 #'   column defines the depth at the center of each depth bin, and the second 
 #'   column defines the half-width of each bin.
-#' @param beta the current depth bin transition model parameters
-#' @param lambda the current depth bin transition rate model parameters
+#' @param beta.seq the range of depth bin transition model parameters
+#' @param lambda.seq the range of depth bin transition rate model parameters
 #' @param s0 the stage for which to compute the transition matrix
-#' @param tstep Time between observations of the CTMC
-#' @param include.raw \code{TRUE} to include raw components of the probability
-#'   transition matrix so that the matrix can be computed for arbitrary 
-#'   timesteps
-#' @param delta If \code{delta>0}, then the observation matrix and raw 
-#'   components computed will be for a transition matrix whose generator is 
-#'   perturbed to allow much faster computation.
+#' @param tstep.seq Range of times between observations of the CTMC
+#' @param m Smoothing polynomial degree; this is the \code{m} argument in the 
+#'   smoothing function, \code{fields::Tps}.
+#' @param verbose \code{TRUE} to output progress of code
 #'   
 #' @example examples/dsdive.obstx.matrix_interpolator.R
 #' 
